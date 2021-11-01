4400b3aab0e8c6f81f17dfc0a3e4f7df6faad820
Oleg <oleg-novikov26@yandex.ru>
2021-11-01 17:39:52 +0300

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,53 +7,17 @@ import { WeatherModel } from './interfaces/weatherModel';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  weatherApp1: WeatherModel[] = [];
-  weatherApp2: WeatherModel[] = [];
-  weatherApp3: WeatherModel[] = [];
-  weatherApp4: WeatherModel[] = [];
-  weatherApp5: WeatherModel[] = [];
-  weatherApp6: WeatherModel[] = [];
-  curDate = new Date().getDate();
-  delCounter: number = 0;
+  SliderObj: Record<string, WeatherModel[]> = {};
   title = 'weather';
-  getWeather(weatherModels: WeatherModel[]) {
-    this.weatherApp1=[]
-    weatherModels.forEach((element) => {
-      let weatherDate = element.dt_txt.split(' ', element.dt_txt.length)[0];
-      let weatherDay = weatherDate.split('-', weatherDate.length)[2];
-      if (+weatherDay == this.curDate) {
-        this.weatherApp1.push(element);
-        this.delCounter = this.weatherApp1.length ;
-      }
-    });
 
-    this.weatherApp2 = weatherModels.slice(
-      this.delCounter,
-      this.delCounter + 8
-    );
-    this.weatherApp3 = weatherModels.slice(
-      this.delCounter + 8,
-      this.delCounter + 16
-    );
-    this.weatherApp4 = weatherModels.slice(
-      this.delCounter + 16,
-      this.delCounter + 24
-    );
-    this.weatherApp5 = weatherModels.slice(
-      this.delCounter + 24,
-      this.delCounter + 32
-    );
-    this.weatherApp6 = weatherModels.slice(
-      this.delCounter + 32,
-      weatherModels.length
-    );
-    console.log(
-      this.weatherApp1,
-      this.weatherApp2,
-      this.weatherApp3,
-      this.weatherApp4,
-      this.weatherApp5,
-      this.weatherApp6
-    );
+  getWeather(weatherModels: WeatherModel[]) {
+    this.SliderObj = {};
+    weatherModels.forEach((element: WeatherModel) => {
+      let date = element.dt_txt.split(' ', element.dt_txt.length)[0];
+      if (!Object.keys(this.SliderObj).includes(date)) {
+        this.SliderObj[date] = [];
+      }
+      this.SliderObj[date].push(element);
+    });
   }
 }
--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,24 +1,26 @@
 import { Component, ViewEncapsulation, Input } from '@angular/core';
 import { SwiperComponent } from 'swiper/angular';
-
 import SwiperCore, { Navigation } from 'swiper';
 import { WeatherModel } from '../interfaces/weatherModel';
+
 
 SwiperCore.use([Navigation]);
 
 @Component({
   selector: 'app-swiper',
   template: `<swiper
-    *ngIf="weatherS1.length"
+    *ngIf="WeatherObj"
     [navigation]="true"
     class="mySwiper"
+
   >
-    <ng-template swiperSlide>
+    <ng-template    *ngFor="let item of WeatherObj| keyvalue" swiperSlide>
       <div class="info">
-        <h3 class="weathertittle">{{ weatherS1[0].dt_txt | date }}</h3>
+        <h3 class="weathertittle">{{ item.key| date }}</h3>
         <div class="infobox">
-          <p class="text" *ngFor="let WeatherModel of weatherS1">
-            {{ WeatherModel.dt_txt | date: ' HH mm' }}<br />{{
+          <p class="text" *ngFor="let WeatherModel of item.value">
+            {{ WeatherModel.dt_txt | date: ' HH mm' }}<br />
+            {{
               WeatherModel.main.temp - Kelvin | number: '1.0-0'
             }}
             C°<br />
@@ -27,84 +29,13 @@ SwiperCore.use([Navigation]);
           </p>
         </div>
       </div> </ng-template
-    ><ng-template swiperSlide>
-      <div class="info">
-        <h3 class="weathertittle">{{ weatherS2[0].dt_txt | date }}</h3>
-        <div class="infobox">
-          <p class="text" *ngFor="let WeatherModel of weatherS2">
-            {{ WeatherModel.dt_txt | date: ' HH mm' }}<br />{{
-              WeatherModel.main.temp - Kelvin | number: '1.0-0'
-            }}
-            C°<br />
-            {{ WeatherModel.weather[0].main }}
-            <app-icon [icon]="WeatherModel.weather[0].main"></app-icon>
-          </p>
-        </div></div></ng-template
-    ><ng-template swiperSlide>
-      <div class="info">
-        <h3 class="weathertittle">{{ weatherS3[0].dt_txt | date }}</h3>
-        <div class="infobox">
-          <p class="text" *ngFor="let WeatherModel of weatherS3">
-            {{ WeatherModel.dt_txt | date: ' HH mm' }}<br />{{
-              WeatherModel.main.temp - Kelvin | number: '1.0-0'
-            }}
-            C°<br />
-            {{ WeatherModel.weather[0].main }}
-            <app-icon [icon]="WeatherModel.weather[0].main"></app-icon>
-          </p>
-        </div></div></ng-template
-    ><ng-template swiperSlide>
-      <div class="info">
-        <h3 class="weathertittle">{{ weatherS4[0].dt_txt | date }}</h3>
-        <div class="infobox">
-          <p class="text" *ngFor="let WeatherModel of weatherS4">
-            {{ WeatherModel.dt_txt | date: ' HH mm' }}<br />{{
-              WeatherModel.main.temp - Kelvin | number: '1.0-0'
-            }}
-            C°<br />
-            {{ WeatherModel.weather[0].main }}
-            <app-icon [icon]="WeatherModel.weather[0].main"></app-icon>
-          </p>
-        </div></div
-    ></ng-template>
-    <ng-template swiperSlide>
-      <div class="info">
-        <h3 class="weathertittle">{{ weatherS5[0].dt_txt | date }}</h3>
-        <div class="infobox">
-          <p class="text" *ngFor="let WeatherModel of weatherS5">
-            {{ WeatherModel.dt_txt | date: ' HH mm' }}<br />{{
-              WeatherModel.main.temp - Kelvin | number: '1.0-0'
-            }}
-            C°<br />
-            {{ WeatherModel.weather[0].main }}
-            <app-icon [icon]="WeatherModel.weather[0].main"></app-icon>
-          </p>
-        </div></div
-    ></ng-template>
-    <ng-template swiperSlide>
-      <div class="info">
-        <h3 class="weathertittle">{{ weatherS6[0].dt_txt | date }}</h3>
-        <div class="infobox">
-          <p class="text" *ngFor="let WeatherModel of weatherS6">
-            {{ WeatherModel.dt_txt | date: ' HH mm' }}<br />{{
-              WeatherModel.main.temp - Kelvin | number: '1.0-0'
-            }}
-            C°<br />
-            {{ WeatherModel.weather[0].main }}
-            <app-icon [icon]="WeatherModel.weather[0].main"></app-icon>
-          </p>
-        </div></div
-    ></ng-template>
+    >
   </swiper>`,
   styleUrls: ['./slider.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
 export class SliderComponent {
-  @Input() weatherS1: WeatherModel[] = [];
-  @Input() weatherS2: WeatherModel[] = [];
-  @Input() weatherS3: WeatherModel[] = [];
-  @Input() weatherS4: WeatherModel[] = [];
-  @Input() weatherS5: WeatherModel[] = [];
-  @Input() weatherS6: WeatherModel[] = [];
+  @Input() WeatherObj: Record<string, WeatherModel[]> = {};
+
   Kelvin: number = 273.15;
 }
